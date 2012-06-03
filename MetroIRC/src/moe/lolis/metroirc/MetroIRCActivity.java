16b54d5e522ebc2d120b6b0a369452fad8466dd9
Guy kun <guydesu@gmail.com>
2012-06-03 15:25:48 +0100

    !Initial Android project Commit

---
+++ MetroIRC/src/moe/lolis/metroirc/MetroIRCActivity.java
@@ -0,0 +1,13 @@
+package moe.lolis.metroirc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MetroIRCActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
